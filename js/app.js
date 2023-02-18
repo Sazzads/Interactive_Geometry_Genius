fbd3ced2619f877d1cbdbc8f0a3bf464b12ea3fc
Sazzads <sazzadtk@gmail.com>
2023-02-18 17:18:57 +0600

    resolve some problem

--- js/app.js
+++ js/app.js
@@ -2,60 +2,124 @@
 //triangle events
 
 document.getElementById('triangle-btn').addEventListener('click', function () {
-   //call input value function
+    //call input value function
     const triangleInput = getinputFieldValueById('triangle-input1', 'triangle-input2');
     const triangleResult = 0.5 * triangleInput;
-    // console.log(triangleResult);
+    if ( isNaN(triangleResult) || triangleResult<0 || triangleResult=='') {
+        alert('Please enter the valid Number');
+        return;
+    }
+    console.log(triangleResult);
+
+    const resultDashboard = document.getElementById('calculation-dashboard');
+    const p = document.createElement('p')
+    p.innerHTML = serialNumber()+". Traingle" + "  " + triangleResult.toFixed(2) + "<span>cm <sup>2</sup></span>" + '   ' + ' ' + "<span style='color: white; background-color: #0B5ED7; padding: 6px 8px; border-radius: 2px;' >Convert to m<sup>2</sup></span>";
+    resultDashboard.appendChild(p);
+
 
     //call result function
-    setTextElementValueById('triangle-calculation', triangleResult);
+    // setTextElementValueById(elementId, triangleResult);
 });
 
 //rectangle events
 document.getElementById('rectangle-btn').addEventListener('click', function () {
-   //call input value function
+    //call input value function
     const rectangleInput = getinputFieldValueById('rectangle-input1', 'rectangle-input2');
+    
+    if ( isNaN(rectangleInput) || rectangleInput<0 || rectangleInput=='') {
+        alert('Please enter the valid Number');
+        return;
+    }
+
+    const resultDashboard = document.getElementById('calculation-dashboard');
+    const p = document.createElement('p')
+    p.innerHTML = serialNumber()+". Rectangle" + "  " + rectangleInput.toFixed(2) + "<span>cm <sup>2</sup></span>" + '   ' + ' ' + "<span style='color: white; background-color: #0B5ED7; padding: 6px 8px; border-radius: 2px;' >Convert to m<sup>2</sup></span>";
+    resultDashboard.appendChild(p);
 
     //call result function
-    setTextElementValueById('rectangle-calculation', rectangleInput);
+    // setTextElementValueById('rectangle-calculation', rectangleInput);
 });
 //Parallelogram events
 document.getElementById('parallelogram-btn').addEventListener('click', function () {
-   //call input value function
+    //call input value function
     const parallelogramInput = getinputFieldValueById('parallelogram-input1', 'parallelogram-input2');
+    if ( isNaN(parallelogramInput) || parallelogramInput<0 || parallelogramInput=='') {
+        alert('Please enter the valid Number');
+        return;
+    }
+
+    const resultDashboard = document.getElementById('calculation-dashboard');
+    const p = document.createElement('p')
+    p.innerHTML = serialNumber()+". Parallelogram" + "  " + parallelogramInput.toFixed(2) + "<span>cm <sup>2</sup></span>" + '   ' + ' ' + "<span style='color: white; background-color: #0B5ED7; padding: 6px 8px; border-radius: 2px;' >Convert to m<sup>2</sup></span>";
+    resultDashboard.appendChild(p);
 
     //call result function
-    setTextElementValueById('parallelogram-calculation', parallelogramInput);
+    // setTextElementValueById('parallelogram-calculation', parallelogramInput);
 });
 
 //Rhombus events
 document.getElementById('rhombus-btn').addEventListener('click', function () {
     //call input value function
-     const rhombusInput = getinputFieldValueById('rhombus-input1', 'rhombus-input2');
-     const rhombusResult = 0.5 * rhombusInput;
-    //  console.log(rhombusResult);
- 
-     //call result function
-     setTextElementValueById('rhombus-calculation', rhombusResult);
- });
+    const rhombusInput = getinputFieldValueById('rhombus-input1', 'rhombus-input2');
+    const rhombusResult = 0.5 * rhombusInput;
+    if ( isNaN(rhombusResult) || rhombusResult<0 || rhombusResult=='') {
+        alert('Please enter the valid Number');
+        return;
+    }
+
+    const resultDashboard = document.getElementById('calculation-dashboard');
+    const p = document.createElement('p')
+    p.innerHTML = serialNumber()+". Rhombus" + "  " + rhombusResult.toFixed(2) + "<span>cm <sup>2</sup></span>" + '   ' + ' ' + "<span style='color: white; background-color: #0B5ED7; padding: 6px 8px; border-radius: 2px;' >Convert to m<sup>2</sup></span>";
+    resultDashboard.appendChild(p);
+
+    //call result function
+    // setTextElementValueById('rhombus-calculation', rhombusResult);
+});
 
 //Pentagon events
 document.getElementById('pentagon-btn').addEventListener('click', function () {
     //call input value function
-     const pentagonInput = getinputFieldValueById('pentagon-input1', 'pentagon-input2');
-     const pentagonResult = 0.5 * pentagonInput;
-    //  console.log(rhombusResult);
- 
-     //call result function
-     setTextElementValueById('pentagon-calculation', pentagonResult);
- });
+    const pentagonInput = getinputFieldValueById('pentagon-input1', 'pentagon-input2');
+    const pentagonResult = 0.5 * pentagonInput;
+    if ( isNaN(pentagonResult) || pentagonResult<0 || pentagonResult=='') {
+        alert('Please enter the valid Number');
+        return;
+    }
+
+    const resultDashboard = document.getElementById('calculation-dashboard');
+    const p = document.createElement('p')
+    p.innerHTML = serialNumber()+". Pentagon" + "  " + pentagonResult.toFixed(2) + "<span>cm <sup>2</sup></span>" + '   ' + ' ' + "<span style='color: white; background-color: #0B5ED7; padding: 6px 8px; border-radius: 2px;' >Convert to m<sup>2</sup></span>";
+    resultDashboard.appendChild(p);
+
+    //call result function
+    // setTextElementValueById('pentagon-calculation', pentagonResult);
+});
 //Ellipse events
 document.getElementById('ellipse-btn').addEventListener('click', function () {
     //call input value function
-     const ellipseInput = getinputFieldValueById('ellipse-input1', 'ellipse-input2');
-     const ellipseResult = 3.14 * ellipseInput;
-    //  console.log(rhombusResult);
- 
-     //call result function
-     setTextElementValueById('ellipse-calculation', ellipseResult);
- });
+    const ellipseInput = getinputFieldValueById('ellipse-input1', 'ellipse-input2');
+    const ellipseResult = 3.14 * ellipseInput;
+    if ( isNaN(ellipseResult) || ellipseResult<0 || ellipseResult=='') {
+        alert('Please enter the valid Number');
+        return;
+    }
+
+    const resultDashboard = document.getElementById('calculation-dashboard');
+    const p = document.createElement('p')
+    p.innerHTML = serialNumber()+". Ellipse" + "  " + ellipseResult.toFixed(2) + "<span>cm <sup>2</sup></span>" + '   ' + ' ' + "<span style='color: white; background-color: #0B5ED7; padding: 6px 8px; border-radius: 2px;' >Convert to m<sup>2</sup></span>";
+    resultDashboard.appendChild(p);
+
+    //call result function
+    // setTextElementValueById('ellipse-calculation', ellipseResult);
+});
+
+//random color
+
+document.querySelector('.random').addEventListener('mouseenter', function () {
+    const colors = 'rgb(' + Math.round(Math.random() * 255) + ',' + Math.round(Math.random() * 255) + ',' + Math.round(Math.random() * 255) + ')';
+    this.style.background = colors;
+})
+document.querySelector('.random2').addEventListener('mouseenter', function () {
+    const colors = 'rgb(' + Math.round(Math.random() * 255) + ',' + Math.round(Math.random() * 255) + ',' + Math.round(Math.random() * 255) + ')';
+    this.style.background = colors;
+})
--- js/function.js
+++ js/function.js
@@ -1,20 +1,41 @@
 
-function getinputFieldValueById(inputField1Id1,inputField2Id2) {
+function getinputFieldValueById(inputField1Id1, inputField2Id2) {
+
+
     const inputField1 = document.getElementById(inputField1Id1);
     const inputField1ValueString = inputField1.value;;
     const inputField1Value = parseFloat(inputField1ValueString);
+
     inputField1.value = '';
 
     const inputField2 = document.getElementById(inputField2Id2);
     const inputField2ValueString = inputField2.value;;
     const inputField2Value = parseFloat(inputField2ValueString);
+    if(inputField2Value<0){
+        alert('Please enter the valid Number');
+        inputField2.value = '';
+
+        return;
+    }
     inputField2.value = '';
 
-    const functionResult=inputField1Value*inputField2Value;
+    const functionResult = inputField1Value * inputField2Value;
     return functionResult;
+
+
 }
 
-function setTextElementValueById(elementId, newValue) {
-    const textElement = document.getElementById(elementId);
-    textElement.innerText = newValue;
+
+let number=0;
+function serialNumber(){
+    number++;
+    return number;
 }
+
+// function setTextElementValueById(elementId, newValue) {
+//     const textElement = document.getElementById(elementId);
+    
+//     // textElement.innerText =newValue;
+//     textElement.innerHTML = elementId +"  " +newValue.toFixed(2) +"<span>cm <sup>2</sup></span>" +'   '+' '+ "<span style='color: white; background-color: #0B5ED7; padding: 6px 8px; border-radius: 2px;' >Convet to m<sup>2</sup></span>";
+// }
+
